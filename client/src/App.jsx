7a13601030ed4312a790c8754e38af0c40eb2d3a
Atsuma <asd463291@gmail.com>
2023-01-04 23:51:53 +0800

    feat: useAsync custom hook

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom"
 import { PostList } from "./components/PostLists"
 
-
 function App() {
-  return <PostList />
+  return (
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<PostList />} />
+        <Route path="/posts/:id" element={null} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
--- client/src/components/PostLists.jsx
+++ client/src/components/PostLists.jsx
@@ -1,16 +1,18 @@
-import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import { useAsync } from "../hooks/useAsync"
 import { getPosts } from "../services/posts"
 
 export function PostList() {
-  const [posts, setPosts] = useState([])
-  useEffect(() => {
-    getPosts().then(setPosts)
-  }, [])
+  const { loading, error, value: posts } = useAsync(getPosts)
+
+  if (loading) return <h1>Loading...</h1>
+
+  if (error) return <h1 className="error-msg">{error.message}</h1>
 
   return posts.map((post) => {
     return (
       <h1 key={post.id}>
-        <a href={`/posts/${post.id}`}>{post.title}</a>
+        <Link to={`/posts/${post.id}`}>{post.title}</Link>
       </h1>
     )
   })
