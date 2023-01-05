4c6f54f7de64d0fdc3b3e1b4b6aca22e53f12781
Atsuma <asd463291@gmail.com>
2023-01-05 11:56:51 +0800

    feat: render comments

--- client/src/contexts/PostContext.jsx
+++ client/src/contexts/PostContext.jsx
@@ -1,13 +1,36 @@
-import React from "react"
+import React, { useMemo } from "react"
+import { useContext } from "react"
 import { useParams } from "react-router-dom"
 import { useAsync } from "../hooks/useAsync"
 import { getPost } from "../services/posts"
 
 const Context = React.createContext()
 
+export function usePost() {
+  return useContext(Context)
+}
+
 export function PostProvider({ children }) {
   const { id } = useParams()
   const { loading, error, value: post } = useAsync(() => getPost(id), [id])
-  console.log(post)
-  return <Context.Provider value={{ post: { id, ...post } }}>{loading ? <h1>Loading</h1> : error ? <h1 className="error-msg">{error}</h1> : children}</Context.Provider>
+
+  const commentsByParentId = useMemo(() => {
+    if (post?.comments == null) return []
+    const group = {}
+    post.comments.forEach((comment) => {
+      group[comment.parentId] ||= []
+      group[comment.parentId].push(comment)
+    })
+    return group
+  }, [post?.comments])
+
+  function getReplies(parentId) {
+    return commentsByParentId[parentId] || []
+  }
+
+  return (
+    <Context.Provider value={{ post: { id, ...post }, getReplies, rootComments: commentsByParentId[null] }}>
+      {loading ? <h1>Loading</h1> : error ? <h1 className="error-msg">{error}</h1> : children}
+    </Context.Provider>
+  )
 }
