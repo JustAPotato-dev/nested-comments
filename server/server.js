0a5b4f7fb461853c1949670a7c88a632d922108a
Atsuma <asd463291@gmail.com>
2023-01-05 13:45:14 +0800

    feat: user authentication

--- server/server.js
+++ server/server.js
@@ -1,17 +1,42 @@
 import fastify from "fastify"
-import fastifySensible from "@fastify/sensible"
-import fastifyCors from "@fastify/cors"
+import sensible from "@fastify/sensible"
+import cors from "@fastify/cors"
+import cookie from "@fastify/cookie"
 import dotenv from "dotenv"
 import { PrismaClient } from "@prisma/client"
 dotenv.config()
 
 const app = fastify()
-app.register(fastifySensible)
-app.register(fastifyCors, {
+app.register(sensible)
+app.register(cookie, { secret: process.env.COOKIE_SECRET })
+app.register(cors, {
   origin: process.env.CLIENT_URL,
   credentials: true,
 })
+app.addHook("onRequest", (req, res, done) => {
+  if (req.cookies.userId !== CURRENT_USER_ID) {
+    req.cookies.userId = CURRENT_USER_ID
+    res.clearCookie("userId")
+    res.setCookie("userId", CURRENT_USER_ID, {
+      domain: process.env.CLIENT_URL,
+    })
+  }
+  done()
+})
 const prisma = new PrismaClient()
+const CURRENT_USER_ID = (await prisma.user.findFirst({ where: { name: "Kyle" } })).id
+const COMMENT_SELECT_FIELDS = {
+  id: true,
+  message: true,
+  parentId: true,
+  createdAt: true,
+  user: {
+    select: {
+      id: true,
+      name: true,
+    },
+  },
+}
 
 app.get("/posts", async (req, res) => {
   return await commitToDb(
@@ -35,20 +60,25 @@ app.get("/posts/:id", async (req, res) => {
           orderBy: {
             createdAt: "desc",
           },
-          select: {
-            id: true,
-            message: true,
-            parentId: true,
-            createdAt: true,
-            user: {
-              select: {
-                id: true,
-                name: true,
-              },
-            },
-          },
+          select: COMMENT_SELECT_FIELDS,
         },
       },
+    })
+  )
+})
+
+app.post("/posts/:id/comments", async (req, res) => {
+  if (req.body.message === "" || req.body.message === null) return app.httpErrors.badRequest("Comment cannot be empty")
+
+  return await commitToDb(
+    prisma.comment.create({
+      data: {
+        message: req.body.message,
+        userId: req.cookies.userId,
+        parentId: req.body.parentId,
+        postId: req.params.id,
+      },
+      select: COMMENT_SELECT_FIELDS,
     })
   )
 })
